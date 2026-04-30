c9a027547a02bfcd4fed51873c081863c0f263c1
Xuyang Han <43256411+xuyangHan@users.noreply.github.com>
2026-04-30 14:59:14 -0400

    remove default created logics

---
+++ PawTrackApp/PawTrackApp/Models/Item.swift
@@ -0,0 +1,11 @@
+//
+//  Item.swift
+//  PawTrackApp
+//
+//  Created by Xuyang Han on 2026-04-29.
+//
+
+import Foundation
+
+// Placeholder type to preserve the default project file structure.
+struct Item {}
---
+++ PawTrackApp/PawTrackApp/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PawTrackApp
+//
+//  Created by Xuyang Han on 2026-04-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            Color.clear
+                .navigationTitle("PawTrack")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
